cf08fe7eda5529b39642676bbff8ebb2baf71eed
amit steinmetz <amitsteinmetz@gmail.com>
2026-02-10 15:05:43 +0200

    mock

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -24,6 +24,7 @@ export class AuthService {
   private currentUserSignal = signal<User | null>(null);
   currentUser = this.currentUserSignal.asReadonly();
   private accessToken: string | null = null;
+  // fwuifhiaf
 
   constructor(private http: HttpClient) { }
 
